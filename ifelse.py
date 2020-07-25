50174e0ee2ae0dad4fea46f6052df01f8c07b0a6
jayaprabha-jayaprabha-04 <68436238+jayaprabha-jayaprabha-04@users.noreply.github.com>
2020-07-25 14:59:16 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+def condition():
+    a = int(input("Enter the value of a :"))
+    b = int(input("Enter the value of b :"))
+    if a<b:
+        print("a<b is True")
+    else:
+        print("a<b is False")
+
+condition()
