a99ed59294777de6c2251838fd3c0460f0553cc2
jayaprabha-jayaprabha-04 <68436238+jayaprabha-jayaprabha-04@users.noreply.github.com>
2020-07-28 16:20:04 +0530

    Add files via upload

---
+++ whileloop statement.py
@@ -0,0 +1,24 @@
+#while loop statements
+
+counter=1
+while counter<=10:
+   print("hello")
+   counter+=1
+
+#nested while loop 
+
+counter=1
+new counter=1
+while counter<=5
+   print("hi")
+   newcounter=1
+   while newcounter<=5
+      print("welcome")
+      newcounter+=1
+  counter+=1
+
+#infinite while loop
+
+counter=1
+while true:
+   print("how are u")
