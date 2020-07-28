f052f686e1dac937c955145ec071545454e64f0f
jayaprabha-jayaprabha-04 <68436238+jayaprabha-jayaprabha-04@users.noreply.github.com>
2020-07-28 16:09:25 +0530

    Add files via upload

---
+++ branching statements.py
@@ -0,0 +1,31 @@
+#types of branching statements
+1. simple if statement
+    a=5
+    if(a==5):
+       print("welcome")
+2.if...else statement
+    a=5
+    if(a==4):
+       print("hi")
+    else:
+       print("hello")
+3.elseif ladder statement
+    a=int(input("enter the value")
+    if(a==4):
+       print("four")
+        elseif(a==5)
+           print("five")
+            elseif("six")
+    else
+       print("zero")
+4.nested if statement
+    a=int(input("enter the value"))
+    b=int(input("enter the value"))
+    if(a==4):
+            if(a>b):
+              print("yes")
+            else
+               print("no")
+    else
+       print("zero")
+  
